92129a543442fab53e1f1a73553be5297ba50000
Daniel Gaston <daniel.gaston@flatironschool.com>
2023-06-01 11:41:28 -0400

    Add improvements to tests, make code not pass tests initially
    In lib/classes
    - assign None to attributes in constructors so that tests don't pass without students doing anything
    - replace pass with return [] in coffees(), customers(), and orders() so that a type error is not thrown in the tests
    In lib/testing
    - remove test_can_change_name which always passes
    - uncomment tests for raising exceptions
    - remove test_get_all_orders, which was not described in the instructions
    - add more descriptive error messages to asserts

--- lib/testing/coffee_test.py
+++ lib/testing/coffee_test.py
@@ -10,19 +10,19 @@ class TestCoffee:
     def test_has_name(self):
         '''coffee is initialized with a name'''
         coffee = Coffee("Mocha")
-        assert (coffee.name == "Mocha")
+        assert (coffee.name == "Mocha"), "The name attribute of Coffee was not set"
 
     def test_name_is_string(self):
         '''coffee is initialized with a name of type str'''
         coffee = Coffee("Mocha")
-        assert (isinstance(coffee.name, str))
+        assert (isinstance(coffee.name, str)), "The name attribute of Coffee was not initialized with a str"
 
     def test_name_setter(self):
         '''Cannot change the name of the coffee'''
         coffee = Coffee("Mocha")
 
-        # with pytest.raises(Exception):
-        #     coffee.name = "Peppermint Mocha"
+        with pytest.raises(Exception):
+            coffee.name = "Peppermint Mocha"
 
     def test_has_many_orders(self):
         '''coffee has many orders.'''
@@ -34,7 +34,7 @@ class TestCoffee:
         order_3 = Order(customer, coffee_2, 5)
 
         assert (len(coffee.orders()) == 2)
-        assert (order_1 in coffee.orders())
+        assert (order_1 in coffee.orders()), "order was not added to the coffee object's order list. Check that coffee.orders() is being called in Order.__init__"
         assert (order_2 in coffee.orders())
         assert (not order_3 in coffee.orders())
 
@@ -45,7 +45,7 @@ class TestCoffee:
         order_1 = Order(customer, coffee, 2)
         order_2 = Order(customer, coffee, 5)
 
-        assert (isinstance(coffee.orders()[0], Order))
+        assert (isinstance(coffee.orders()[0], Order)), "Coffee.orders() should return a list with objects of type Order" 
         assert (isinstance(coffee.orders()[1], Order))
 
     def test_has_many_customers(self):
@@ -57,7 +57,7 @@ class TestCoffee:
         order_1 = Order(customer, coffee, 2)
         order_2 = Order(customer_2, coffee, 5)
 
-        assert (customer in coffee.customers())
+        assert (customer in coffee.customers()), "customer was not added to the coffee object's customer list. Check that coffee.customers() is being called in Order.__init__"
         assert (customer_2 in coffee.customers())
 
     def test_has_unique_customers(self):
@@ -69,9 +69,10 @@ class TestCoffee:
         order_1 = Order(customer, coffee, 2)
         order_2 = Order(customer_2, coffee, 2)
         order_3 = Order(customer, coffee, 5)
-
-        assert (len(set(coffee.customers())) == len(coffee.customers()))
-        assert (len(coffee.customers()) == 2)
+        
+        assert (len(coffee.customers()) == 2), "the Coffee.customers() method did not return a list of 2 customers"
+        assert (len(set(coffee.customers())) == len(coffee.customers())), "Duplicate customers found in in Coffee"
+        
 
     def test_customers_of_type_customer(self):
         '''coffee customers are of type Customer'''
@@ -81,7 +82,7 @@ class TestCoffee:
         order_1 = Order(customer, coffee, 2)
         order_2 = Order(customer_2, coffee, 5)
 
-        assert (isinstance(coffee.customers()[0], Customer))
+        assert (isinstance(coffee.customers()[0], Customer)), "Coffee.customers() should return a list with objects of type Customer" 
         assert (isinstance(coffee.customers()[1], Customer))
 
     def test_get_number_of_orders(self):
@@ -91,7 +92,7 @@ class TestCoffee:
         order_1 = Order(customer, coffee, 2)
         order_2 = Order(customer, coffee, 5)
 
-        assert (coffee.num_orders() == 2)
+        assert (coffee.num_orders() == 2), "num_orders does not return the correct number. Check that Order.__init__ is calling coffee.orders()"
 
     def test_average_price(self):
         '''test average_price()'''
@@ -101,4 +102,4 @@ class TestCoffee:
         Order(customer, coffee, 2)
         Order(customer_2, coffee, 5)
 
-        assert (coffee.average_price() == 3.5)
+        assert (coffee.average_price() == 3.5), "average_price() computes incorrect result"
